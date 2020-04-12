5aedea5298b9a254108e23b604a8b1d91abcc244
alpha-256 <alpha-256@users.noreply.github.com>
2020-04-12 16:51:57 +0800

    Attempt
    Attempted to add feature...

--- TERMINAL/main.py
+++ TERMINAL/main.py
@@ -1,12 +1,11 @@
 from random import randint as ri
 from random import choice as rc
-from pprint import pprint
 
-neuralMatrix = []
-rowIOLog = []
-columnIOLog = []
+pathMatrix = []
+IOLog = []
 cellTypes = ["I", "O"]
 pathTypeCells = ["G", "R"]
+
 """
 I = input   Cell
 O = Output  Cell
@@ -16,64 +15,73 @@ R = Red     Cell    (Decrease Signal per step from I Cell)
 
 def createCleanMatrix(layerCount, size):
     for layer in range(layerCount):
-        neuralMatrix.append([])
-    #print(neuralMatrix)
+        pathMatrix.append([])
+    #print(pathMatrix)
 
-    for layer in range(len(neuralMatrix)):
+    for layer in range(len(pathMatrix)):
         for index in range(size):
-            neuralMatrix[layer].append([])
-    #print(neuralMatrix)
+            pathMatrix[layer].append([])
+    #print(pathMatrix)
 
-    for layer in range(len(neuralMatrix)):
+    for layer in range(len(pathMatrix)):
         for index in range(size):
             for item in range(size):
-                neuralMatrix[layer][item].append("O")
-    #print(neuralMatrix, "\n")
+                pathMatrix[layer][item].append("O")
+    #print(pathMatrix, "\n")
 
 def createFirstInput():
     layer = 0
-    x = int(ri(1, size-1))
-    y = int(ri(1, size-1))
-    rowIOLog.append(x)
-    columnIOLog.append(y)
-    neuralMatrix[layer][x][y] = "I"
+    index = int(ri(1, size-1))
+    item = int(ri(1, size-1))
+    pathMatrix[layer][index][item] = "I"
 
 def createPaths():
-    locateX = int()
-    locateY = int()
-    for layer in range(len(neuralMatrix)):
+    for layer in range(len(pathMatrix)):
         for index in range(size):
             for item in range(size):
-
-                cell = neuralMatrix[layer][index][item]
+                cell = pathMatrix[layer][index][item]
                 if cell == "O":
                     change = str(rc(pathTypeCells))
-                    neuralMatrix[layer][index][item] = change
+                    pathMatrix[layer][index][item] = change
                 else:
                     pass
 
-def connectLayerIO():
-    index  = 0
-    matrixLayers = len(neuralMatrix)
-    while index < matrixLayers:
-        layer = index
-        x = rowIOLog[index]
-        y = columnIOLog[index]
-        neuralMatrix[layer][x][y] = "I"
-        index += 1
+def create3OutputCells():
+    tempLog = []
+    for layer in range(len(pathMatrix)):
+        tempLog.append([])
+    for iteration in range(3):
+        for layer in range(len(pathMatrix)):
+            index = ri(0, size - 1)
+            item = ri(0, size - 1)
+            pathMatrix[layer][index][item] = "O"
+            tempLog[layer].append([index, item])
+    for layer in range(len(tempLog)):
+        IOLog.append(tempLog[layer])
+
+def connectIO():
+    for layer in range(len(IOLog)):
+        for index in range(len(IOLog[layer])):
+            for coordinates in range(len(IOLog[layer][index])):
+                print(IOLog[layer][index][0])
+                print(IOLog[layer][index][1])
+            print("\n")
+
+    #tempYIndex = yLog[len(yLog)]
+    #tempXIndex = xLog[len(xLog)]
 
 def debugPrint():
-    #print(neuralMatrix)
-    for layer in range(len(neuralMatrix)):
+    #print(pathMatrix)
+    for layer in range(len(pathMatrix)):
         for row in range(size):
-            print(neuralMatrix[layer][row])
+            print(pathMatrix[layer][row])
         print("\n")
 
 def cleanPrint():
-    for layer in range(len(neuralMatrix)):
+    for layer in range(len(pathMatrix)):
         tempData = []
         for row in range(size):
-            tempData.append(" ".join(neuralMatrix[layer][row]))
+            tempData.append(" ".join(pathMatrix[layer][row]))
         for item in tempData:
             print("{:>1}".format(item))
         print("\n")
@@ -82,21 +90,22 @@ layerCount = int(input("Amount of layers: "))
 size = int(input("Size: "))
 
 createCleanMatrix(layerCount, size)
-debugPrint()
 createFirstInput()
-print("Before", "\n")
-print("###############", "\n")
-print("After", "\n")
-debugPrint()
-print("\n")
-cleanPrint()
+
 createPaths()
-print("Before", "\n")
-print("###############", "\n")
-print("After", "\n")
-debugPrint()
-print("\n")
+create3OutputCells()
+
 cleanPrint()
+print(IOLog)
+
+connectIO()
+
+cleanPrint()
+print(" Before \n ###### \n After \n")
+cleanPrint()
+print(IOLog)
+
+debugPrint()
 
 
 
